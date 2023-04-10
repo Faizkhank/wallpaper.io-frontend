e5ba9001d524551af84df30482604dd542ece1bb
faiz khan <fine7production@gmail.com>
2023-04-10 19:41:49 +0530

    c

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -174,7 +174,29 @@ export default function Card(props) {
                       </button>
                     )}
                   </div>
-                ) : null}
+                ) : (
+                  <button
+                    type="button"
+                    className={`mr-7 mb-4 bg-white w-14 h-10 rounded-lg ${
+                      button ? "button" : "hidden"
+                    }`}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth="1.5"
+                      stroke="currentColor"
+                      className="w-6 h-6 m-auto"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3"
+                      />
+                    </svg>
+                  </button>
+                )}
               </div>
             </div>
           ) : null}
