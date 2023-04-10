63bde4fb4d589891fc4e45434028d7d92ee80838
faiz khan <fine7production@gmail.com>
2023-04-11 00:35:56 +0530

    d

--- src/component/services/ContextAuth.js
+++ src/component/services/ContextAuth.js
@@ -81,7 +81,7 @@ export const AuthContextProvider = ({ children }) => {
     try {
       axios
         .get(
-          `https://api-wallpaper-io.onrender.com/api/search?q=${searchquery}&page?p=${pageNumber}`,
+          `https://api-wallpaper-io.onrender.com/api/page_0/search?q=${searchquery}&page?p=${pageNumber}`,
           {
             withCredentials: true,
             headers: {
