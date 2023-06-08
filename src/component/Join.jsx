23b0aea24fb334e33bc7406528b914382c9ddad2
faiz khan <fine7production@gmail.com>
2023-06-08 17:30:35 +0530

    commit

--- src/component/Join.jsx
+++ src/component/Join.jsx
@@ -41,12 +41,7 @@ const Login = () => {
       .then((res) => {
         setprocess(false);
         if (res.data.success === true) {
-          // Save the session cookie
-          const sessionCookie = res.headers["set-cookie"];
-          console.log(sessionCookie);
-          if (sessionCookie) {
-            document.cookie = sessionCookie;
-          }
+          setUser(res.data);
           navigate("/");
         } else {
           setresponse(res.data);
